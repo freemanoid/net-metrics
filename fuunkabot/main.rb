f662b2eb5ea800390b458ec85a2bce9eb9f2f3f9
freemanoid <freemanoid321@gmail.com>
2017-10-13 17:37:18 +0300

    Let's use Excon for HTTP

--- fuunkabot/main.rb
+++ fuunkabot/main.rb
@@ -1,7 +1,7 @@
 require 'bundler'
 Bundler.require
 
-require 'net/http'
+require 'excon'
 require 'json'
 
 TOKEN = "463417531:AAGtmovT08ni1WaNRAvFo_rveVmx4jpTHR4"
@@ -9,15 +9,18 @@ TOKEN = "463417531:AAGtmovT08ni1WaNRAvFo_rveVmx4jpTHR4"
 GET_UPDATES_URL = "https://api.telegram.org/bot#{TOKEN}/getUpdates"
 SEND_MESSAGE_URL = "https://api.telegram.org/bot#{TOKEN}/sendMessage"
 
+
 def get_updates(offset: 0)
-  uri = URI(GET_UPDATES_URL)
-  uri.query = URI.encode_www_form(timeout: 0, offset: offset)
-  response = Net::HTTP.get(uri)
-  JSON.parse(response)
+  response = Excon.new(GET_UPDATES_URL).get(query: { timeout: 0, offset: offset })
+  JSON.parse(response.body)
 end
 
 def send_message(chat_id:, text:)
-  response = Net::HTTP.post(URI(SEND_MESSAGE_URL), { chat_id: chat_id, text: text }.to_json, "Content-Type" => "application/json")
+  response = Excon.post(SEND_MESSAGE_URL,
+    body: { chat_id: chat_id, text: text }.to_json,
+    headers: { "Content-Type" => "application/json" }
+  )
+
   response_body = JSON.parse(response.body)
   raise response_body unless response_body["ok"]
 end
@@ -30,6 +33,7 @@ def get_temp
   42
 end
 
+@offset = 0
 loop do
   updates = get_updates(offset: @offset)
   @offset = 0
